293f09032cc1e1ea816f19dddb3c35975db788a2
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-03-25 12:50:36 +0300

    Added addition popup demo.

--- front/src/components/NutritionChart/NutritionChart.tsx
+++ front/src/components/NutritionChart/NutritionChart.tsx
@@ -14,6 +14,22 @@ export default function NutritionChart() {
                 <NutritionChartValue type={"fats"} value={20} maxValue={100}></NutritionChartValue>
                 <NutritionChartValue type={"carbs"} value={10} maxValue={200}></NutritionChartValue>
             </div>
+            <button>+</button>
+            <div className="chart__popup">
+                <input type="text" placeholder="Название" />
+                <input type="number" placeholder="Белки на 100 грамм"/>
+                <input type="number" placeholder="Жиры на 100 грамм" />
+                <input type="number" placeholder="Углеводы на 100 грамм" />
+                <input type="number" placeholder="ККал на 100 грамм"/>
+                <input type="number" placeholder="Грамм"/>
+
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
         </div>
     )
 }
